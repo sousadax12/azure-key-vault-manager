ae7acbcd33bcd2cfcca970f27c1e988924cc4dad
Joao Sousa <sousadax@gmail.com>
2021-05-24 22:52:35 +0100

    Create spike api and add nedb to store multiple key vaults

---
+++ src/server/keyVaultsRepository.ts
@@ -0,0 +1,29 @@
+import * as Datastore from 'nedb';
+import * as os from 'os';
+
+const homedir = os.homedir();
+const db = new Datastore({ filename: `${homedir}/.azkvm/vaults.db` });
+db.loadDatabase(function (err) {
+  console.error(err);
+});
+
+export const getKeyVaults = () => new Promise((resolve, reject) => {
+  console.log(db);
+  db.find({}, (err, docs) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(docs);
+    }
+  });
+});
+
+export const createKeyVault = (keyVault) => new Promise((resolve, reject) => {
+    db.update({ name: keyVault.name }, keyVault, { upsert: true }, function (err, numReplaced, upsert) {
+        if (err) {
+            reject(err);
+        } else {
+            resolve({ replaced: numReplaced } );
+        }
+    });
+});
