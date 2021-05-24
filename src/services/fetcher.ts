63b547cc79d9469b30008f004b76310a3ed6ce2c
Pedro Figueiredo <klisarkk@gmail.com>
2021-05-24 23:40:07 +0200

    chore: add axios

---
+++ src/services/fetcher.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+const instance = axios.create({
+  baseURL: 'localhost:3000',
+});
+
+export type FetcherConfig = AxiosRequestConfig;
+
+const fetcher = async <T>(url: string, options?: FetcherConfig): Promise<T> => {
+  const { headers, ...axiosOptions } = options || {};
+
+  const parsedOptions = {
+    ...axiosOptions,
+    headers: {
+      ...(headers || {}),
+    },
+  };
+
+  const currentInstance = instance;
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const res = await currentInstance(url, parsedOptions);
+    return res?.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { instance as axios };
+export default fetcher;
